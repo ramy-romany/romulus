abe1769a18e33a9cd882259dbe02f017219f4bd5
Ramy Romany <ramyromany@Ramys-iMac.local>
2026-05-27 05:20:26 -0700

    Remove backup folder with stale package lock

--- bu/lib/game-engine/tests.ts
+++
@@ -1,9 +0,0 @@
-import { optimizeSettlement } from './settlement';
-
-const payments = optimizeSettlement([
-  { userId: 'a', name: 'A', netCents: -30000 },
-  { userId: 'b', name: 'B', netCents: 20000 },
-  { userId: 'c', name: 'C', netCents: -5000 },
-  { userId: 'd', name: 'D', netCents: 15000 }
-]);
-console.log(payments);
